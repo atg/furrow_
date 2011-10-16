765ce13104d5e9db5c1ef9e6bceca0c7df591422
Alex Gordon <anything@fileability.net>
2011-10-16 17:56:01 +0100

    Now with lexer, codegen

--- source/codegen.cc
+++ source/codegen.cc
@@ -1,6 +1,8 @@
 #import "codegen.hh"
 
-// template<typename T>
 void codegen(ast_t* ast, std::ostream& output) {
+    
+}
+void codegen(expr_t* expr, std::ostream& output) {
     
 }
--- source/codegen.hh
+++ source/codegen.hh
@@ -3,3 +3,4 @@
 #import "ast.hh"
 
 void codegen(ast_t* ast, std::ostream& output);
+void codegen(expr_t* expr, std::ostream& output);
