ba72653fadfb75ab2d24389a635255302a5c7fc2
Alex Gordon <alextgordon@gmail.com>
2011-10-16 16:13:43 +0100

    Added a bit of a grammar

--- source/main.cc
+++ source/main.cc
@@ -1,8 +1,20 @@
-#include <stream>
+#import <iostream>
+
+#import "furrow.hh"
+#import "lexer.hh"
+#import "parser.hh"
+#import "codegen.hh"
 
 int main(int argc, char** argv) {
     
     // Read from argv[1], write to stdout
+    std::ifstream in_f = std::ifstream(argv[1]);
+    std::string in_str = std::string( istreambuf_iterator(in_f), istreambuf_iterator() );
+    std::ostream out_f = std::cout;
+    
+    std::vector<token_t> tokens = lex(in_str);
+    ast_t ast = parse(tokens);
+    codegen(ast, out_f);
     
     return 0;
 }
